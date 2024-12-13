e3b5983592134f0a59e2d84da47ac9f2f70cea6c
Eiior <annadici81@gmail.com>
2024-12-13 08:33:33 +0100

    Page + css

---
+++ PAGES/PageConnexion.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+
+//Compte acceptés
+$login1 = 'Girafe';
+$mdp1 = 'Savane'; 
+$errorMessage = "";
+
+//Verif Login et mdp ont quelque chose dedans
+if (isset($_POST['login']) && isset($_POST['mdp'])) {
+    // Verif si id ET mdp sont corrects
+    if ($_POST['login'] == $login1 && $_POST['mdp'] == $mdp1) {
+        $_SESSION['login'] = $_POST['login'];
+        // Début de la phase de "connexion"
+        $_SESSION['connecter'] = true;
+
+        header('Location: PageAccueil.php');      //Lien quand utilisateur est connecté & déconnecté
+        exit; 
+    } else { //Si id ou MDP incorrect
+        $errorMessage = "Identifiant ou mot de passe incorrect";
+        $_SESSION['connecter'] = false;
+    }
+}
+
+// Si appuit sur deconnection ET suppression des cookies
+if (isset($_POST['deco'])) {
+    $_SESSION['connecter'] = false;
+    session_destroy();
+    header('Location: Connexion.php');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="Projet.css" rel="stylesheet">
+    <title>Connexion</title>
+</head>
+<body>
+    <div class="co1">    
+        <h1>Connexion</h1>
+    
+    <!-- Si session est connecté, ressortir le nom -->
+        <?php if (isset($_SESSION['connecter']) && $_SESSION['connecter'] == true): ?>
+            <p>Vous êtes connecté en tant que <?php echo $_SESSION['login']; ?>.</p>
+        
+            <!-- Bouton déconnecter -->
+            <form method="post" action="">
+                <button type="submit" name="deco">Se déconnecter</button>
+            </form>
+        <?php else: ?>
+        
+            <form method="post" action="">
+                <label for="login">Login :</label>
+                <input type="text" id="login" name="login" required><br>
+                <label for="mdp">Mot de passe :</label>
+                <input type="password" id="mdp" name="mdp" required><br><br>
+    <!-- Affichage du message d'erreur si présent -->
+    <?php if (!empty($errorMessage)): ?>
+        <p style="color: red;"><?php echo $errorMessage; ?></p>
+    <?php endif; ?>
+
+                <button type="submit">Envoyer</button> 
+            </form>
+        
+        <?php endif; ?>
+    </div>
+</body>
+</html>
